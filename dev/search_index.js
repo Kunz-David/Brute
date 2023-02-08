499f5cd8e4e483e1953f7196a45583155f394b63
Documenter.jl <documenter@juliadocs.github.io>
2023-02-08 12:31:39 +0000

    build based on 823b9a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Brute","category":"page"},{"location":"#Brute","page":"Home","title":"Brute","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Brute.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Brute]","category":"page"}]
+}
